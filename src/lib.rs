c2d22b84b2699c312a7b57428c267acac1b2f9ed
fallenhh <fallen2hh@gmail.com>
2020-06-30 01:27:02 +0800

    prototyping

---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+pub struct KvStore{
+
+}
+
+impl KvStore {
+    pub fn new() -> KvStore {
+        KvStore {}
+    }
+
+    pub fn set(&self, k: String, v: String) {}
+
+    pub fn remove(&self, k: String) {}
+
+    pub fn get(&self, k:String) -> Option<String> {
+        Option::None
+    }
+}
