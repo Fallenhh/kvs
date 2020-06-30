ef673c2516767f03e802caaa569e8ab1ee048733
fallenhh <fallen2hh@gmail.com>
2020-07-01 00:48:35 +0800

    impl get

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,11 @@ use std::fs::{File, OpenOptions};
 use std::io::{Write, BufRead, BufReader, Seek, SeekFrom};
 use serde::{Serialize, Deserialize};
 use serde_json;
-pub use anyhow::Result;
+use std::process::exit;
+pub use anyhow::{Result, Error};
+
+#[derive(Debug, Clone)]
+struct KeyNotFoundError;
 
 pub struct KvStore{
     log: File,
@@ -74,12 +78,37 @@ impl KvStore {
         }
         else {
             println!("Key not found");
-            panic!()
+            exit(1);
         }
         Ok(())
     }
 
-    pub fn get(&self, k:String) -> Result<Option<String>> {
-        Ok(None)
+    pub fn get(&mut self, key:String) -> Result<Option<String>> {
+        self.log.seek(SeekFrom::Start(0))?;
+        let br = BufReader::new(&self.log);
+        let mut value = String::new();
+        for line in br.lines() {
+            let line = line.unwrap();
+            let command: Command = serde_json::from_str(line.as_str())?;
+            match command {
+                Command::Set{k ,v} => {
+                    if k == key {
+                        value = v.clone();
+                    }
+                }
+
+                Command::Rm{k} => {
+                    if k == key {
+                        value = String::new();
+                    }
+                }
+            }
+        }
+        if value.is_empty(){
+            Ok(None)
+        }
+        else {
+            Ok(Some(value))
+        }
     }
 }
