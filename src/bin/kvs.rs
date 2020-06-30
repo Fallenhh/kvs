e4e9280dd9eecc70c78301ceed19e3c667fda963
fallenhh <fallen2hh@gmail.com>
2020-06-30 22:55:49 +0800

    impl set & rm

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,7 +1,10 @@
 use clap::{App};
 use std::process::exit;
+use std::path::PathBuf;
+use kvs::KvStore;
+use anyhow::Result;
 
-fn main() {
+fn main() -> Result<()>{
     let matches = App::new("kvs")
                   .version(env!("CARGO_PKG_VERSION"))
                   .author(env!("CARGO_PKG_AUTHORS"))
@@ -15,19 +18,22 @@ fn main() {
                     .arg("<KEY>"))
                   .get_matches();
 
+    let mut store = KvStore::open(".")?;
     match matches.subcommand() {
-        ("set", Some(_matches)) => {
-            eprint!("unimplemented");
-            exit(1);
+        ("set", Some(matches)) => {
+            let key = matches.value_of("KEY").unwrap().to_owned();
+            let value = matches.value_of("VALUE").unwrap().to_owned();
+            store.set(key, value)?;
         }
         ("get", Some(_matches)) => {
             eprint!("unimplemented");
             exit(1);
         }
-        ("rm", Some(_matches)) => {
-            eprint!("unimplemented");
-            exit(1);
+        ("rm", Some(matches)) => {
+            let key = matches.value_of("KEY").unwrap().to_owned();
+            store.remove(key)?;
         }
         _ => panic!()
     }
+    Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,81 @@
 use std::path::PathBuf;
-use anyhow::Result;
+use std::fs::{File, OpenOptions};
+use std::io::{Write, BufRead, BufReader, Seek, SeekFrom};
+use serde::{Serialize, Deserialize};
+use serde_json;
+pub use anyhow::Result;
 
 pub struct KvStore{
+    log: File,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+enum Command{
+    Set {
+        k: String,
+        v: String,
+    },
+    // Get {
+    //     k: String,
+    // },
+    Rm {
+        k: String,
+    }
 }
 
 impl KvStore {
 
-    pub fn open(path: impl Into<PathBuf>) -> Result<KvStore> {
+    pub fn open<T: Into<PathBuf>>(path: T) -> Result<KvStore> {
+        let mut pathbuf = path.into();
+        pathbuf.push("data");
         Ok(KvStore {
+            log: OpenOptions::new()
+                             .read(true)
+                             .append(true)
+                             .create(true)
+                             .open(pathbuf.as_path())?
         })
     }
 
     pub fn set(&mut self, k: String, v: String) ->Result<()>{
+        let payload = Command::Set{k ,v};
+        let serialized_payload = serde_json::to_string(&payload)?;
+
+        writeln!(self.log, "{}", &serialized_payload)?;
         Ok(())
     }
 
-    pub fn remove(&mut self, k: String) -> Result<()>{
+    pub fn remove(&mut self, key: String) -> Result<()>{
+        self.log.seek(SeekFrom::Start(0))?;
+        let br = BufReader::new(&self.log);
+        let mut rm_flag = false;
+        for line in br.lines() {
+            let line = line.unwrap();
+            let command: Command = serde_json::from_str(line.as_str())?;
+            match command {
+                Command::Set{k ,v} => {
+                    if k == key {
+                       rm_flag = true;
+                    }
+                }
+
+                Command::Rm{k} => {
+                    if k == key {
+                       rm_flag = false;
+                    }
+                }
+            }
+        }
+
+        if rm_flag {
+            let payload = Command::Rm{k: key};
+            let serialized_payload = serde_json::to_string(&payload)?;
+            writeln!(self.log, "{}", &serialized_payload)?;
+        }
+        else {
+            println!("Key not found");
+            panic!()
+        }
         Ok(())
     }
 
